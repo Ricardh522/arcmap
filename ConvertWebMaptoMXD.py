63147d1a305d604de6f6225eaad6cdcc8ec6c9cc
Richard Hughes <richardh522@gmail.com>
2017-02-09 21:23:18 -0500

    adding arcmap printing

--- ConvertWebMaptoMXD.py
+++ ConvertWebMaptoMXD.py
@@ -2,15 +2,36 @@ import arcpy
 from arcpy import mapping
 import json
 import os
+import sys
 
-mapfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tests/fixtures/webmap.json")
+#Test values
+# mapfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tests/fixtures/webmap.json")
 template = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tests/templates/RTAA.mxd")
+# file = open(mapfile, 'r')
+# webmap = file.read()
 
-file = open(mapfile, 'r')
-webmap = file.read()
 
-result = mapping.ConvertWebMapToMapDocument(webmap, template)
-mxd = result.mapDocument
+class MXDConvert:
+    def __init__(self, json, layout, format):
+        self.webmap = json
+        self.layout = layout
+        self.format = format
+        pass
 
-outfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output/WebMapMXD.pdf")
-mapping.ExportToPDF(mxd, outfile)
+    def process_template(self):
+
+        result = mapping.ConvertWebMapToMapDocument(self.webmap, template)
+        mxd = result.mapDocument
+
+        outfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output/WebMapMXD.pdf")
+        mapping.ExportToPDF(mxd, outfile)
+        out_bytes = open(outfile, 'rb')
+        return out_bytes.read()
+
+if __name__ == "__main__":
+    webmap = sys.argv[1]
+    layout = sys.argv[2]
+    format = sys.argv[3]
+    conv = MXDConvert(webmap, layout, format)
+    print conv.process_template()
+
