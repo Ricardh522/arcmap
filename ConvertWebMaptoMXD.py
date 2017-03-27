4e6a41e190f5d71fa71d97da66e2125ef4be93b5
Richard Hughes <richardh522@gmail.com>
2017-03-27 09:23:17 -0400

    moving out of production back to staging

--- ConvertWebMaptoMXD.py
+++ ConvertWebMaptoMXD.py
@@ -33,5 +33,60 @@ if __name__ == "__main__":
     layout = sys.argv[2]
     format = sys.argv[3]
     conv = MXDConvert(webmap, layout, format)
-    print conv.process_template()
+    print(conv.process_template())
 
+"""
+This is the django view to use for this tool
+
+
+@api_view(['POST'])
+# @renderer_classes((JSONPRenderer,))
+@authentication_classes((AllowAny,))
+@ensure_csrf_cookie
+def print_mxd(request, format=None):
+    v = system_paths()
+    arcmap_path = v["arcmap_path"]
+    mxd_script = v["mxd_script"]
+
+    username = get_username(request)
+    data = request.POST
+    webmap = data['Web_Map_as_JSON']
+    out_folder = os.path.join(MEDIA_ROOT, username)
+    if not os.path.exists(out_folder):
+        os.mkdir(out_folder)
+    os.chdir(out_folder)
+
+    temp_file = open('webmap.json', 'w')
+    temp_file.write(webmap)
+    temp_file.close()
+
+    format = data['Format']
+    layout_template = data['Layout_Template']
+
+    args = [arcmap_path, mxd_script, '-username', username, '-media', MEDIA_ROOT, '-layout', layout_template, '-format', format]
+    proc = subprocess.Popen(args, executable=arcmap_path, stderr=PIPE, stdout=PIPE)
+    out, err = proc.communicate()
+
+    response = Response()
+    # This format must be identical to the DataFile object returned by the esri print examples
+    host = request.META["HTTP_HOST"]
+
+    if host == "127.0.0.1:8080":
+        protocol = "http"
+    else:
+        protocol = "https"
+
+    url = "{}://{}/media/{}/{}".format(protocol, request.META["HTTP_HOST"], username, "layout.pdf")
+
+    response.data = {
+        "messages": [],
+        "results": [{
+            "value": {
+                "url": url
+            },
+            "paramName": "Output_File",
+            "dataType": "GPDataFile"
+        }]
+    }
+    return response
+"""
